050edb544fb31134fa30221f59946f8a9fd4213a
Akib <akib.rhast@gmail.com>
2019-09-27 17:28:12 -0400

    Example image upload added

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,14 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from os.path import join, dirname, realpath
+
+UPLOAD_FOLDER = join(dirname(realpath(__file__)), 'static')
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,10 @@ from app import app, db
 from app.forms import LoginForm, AddRecipe, AddTag, SelectTag
 from app.models import Recipe, Tag, recipeTag
 from app import Config
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
 
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 @app.route('/')
 @app.route('/index')
@@ -108,4 +111,35 @@ def update_recipe(recipe_id):
 
 
 
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/upload_file', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            uploadedfiledebug = app.config['UPLOAD_FOLDER']
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return render_template('image_upload.html')
 
