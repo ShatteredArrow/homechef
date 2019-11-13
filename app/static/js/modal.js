65d20fa1bb420cca012f87dec5da85c15ffbea17
akibr <akibr@DESKTOP-J5FBATK>
2019-11-12 19:21:19 -0500

    moved buttons to navbar

--- app/static/js/modal.js
+++ app/static/js/modal.js
@@ -2,10 +2,12 @@
 // Get the modal
 var addRecipeModal = document.getElementById("myModal");
 var addTagModal = document.getElementById("addTagModal");
+var advancedSearchModal = document.getElementById("advancedSearchModal")
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 var addTagButton = document.getElementById("addTagButton");
+var advancedSearchButton = document.getElementById("advancedSearchButton");
 
 // Get the <span> element that closes the modal
 var addRecipeClose = document.getElementById("addRecipeClose");
@@ -35,6 +37,19 @@ if (addTagModal !== null){
     }
 }
 
+
+if (advancedSearchModal !== null){
+    // When the user clicks the button, open the modal 
+    advancedSearchButton.onclick = function() {
+        advancedSearchModal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    advancedSearchClose.onclick = function() {
+        advancedSearchModal.style.display = "none"
+    }
+}
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == addRecipeModal) {
@@ -42,5 +57,8 @@ window.onclick = function(event) {
     } 
     if (event.target == addTagModal) {
         addTagModal.style.display = "none";
+    }
+    if (event.target == advancedSearchModal) {
+        advancedSearchModal.style.display = "none";
     }
 }
